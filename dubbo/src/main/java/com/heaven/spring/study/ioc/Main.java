253440d10818d768a115047814c102b06150bb08
yuchao.zhang <yuchao.zhang@dmall.com>
2017-08-05 14:34:58 +0800

    测试dubbo接口

--- dubbo/src/main/java/com/heaven/spring/study/ioc/Main.java
+++ dubbo/src/main/java/com/heaven/spring/study/ioc/Main.java
@@ -3,12 +3,15 @@ package com.heaven.spring.study.ioc;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-    	int i=0;
+    	/*int i=0;
     	while(i++<100){
     		ChatAction act = new ChatAction();
     		act.SayHello();
     		Thread.sleep(3000);
-    	}
+    	}*/
+
+    	PushAction pushAction = new PushAction();
+    	pushAction.pushSend();
     }
 
 }
